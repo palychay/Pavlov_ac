1e2b42b1dcaee95b2d10a6506bf407c5f1538360
palychay <118565996+palychay@users.noreply.github.com>
2023-09-02 16:52:40 +0300

    hello world and in/output

---
+++ project_pavlov/pavlov.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello world\n";
+    int n;
+    cout << "input: ";
+    cin >> n;
+    cout << "output: " << n;
+    return 0;
+}
